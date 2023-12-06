3edd663f3f65b55263b3708729a2f07e637af7e6
ani <anirudh.margam@gmail.com>
2023-12-06 10:05:30 -0600

    should be able to grab admin users from DB, have not tested yet, will not merge yet

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,38 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Hardcoded user data
-    var users = [
-        {
-            name: "John Doe",
-            email: "johndoe@example.com",
-            role: "Administrator",
-            address: "1234 Street, City",
-            phoneNumber: "123-456-7890"
-        }
-        // More hardcoded users can be added here
-    ];
-
-    var usersTable = document.getElementById('usersTable');
-    users.forEach(user => {
-        var row = usersTable.insertRow();
-        row.innerHTML = `
-            <td>${user.name}</td>
-            <td>${user.email}</td>
-            <td>${user.role}</td>
-            <td>${user.address}</td>
-            <td>${user.phoneNumber}</td>
-            <td>
-                <button class="btn btn-primary btn-sm">Modify</button>
-                <button class="btn btn-warning btn-sm">Soft Delete</button>
-                <button class="btn btn-danger btn-sm">Hard Delete</button>
-            </td>
-        `;
+    fetch('fetch_users.php') // PHP script that returns user data from the database
+    .then(response => response.json())
+    .then(users => {
+        var usersTable = document.getElementById('usersTable');
+        users.forEach(user => {
+            var row = usersTable.insertRow();
+            row.innerHTML = `
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.role}</td>
+                <td>${user.address}</td>
+                <td>${user.phoneNumber}</td>
+                <td>
+                    <button class="btn btn-primary btn-sm">Modify</button>
+                    <button class="btn btn-warning btn-sm">Soft Delete</button>
+                    <button class="btn btn-danger btn-sm">Hard Delete</button>
+                </td>
+            `;
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching users:', error);
     });
-
-    // Event listener for Add User button
-    document.getElementById('addUserBtn').addEventListener('click', function() {
-        // Logic to add a new user
-    });
-
-    // Add event listeners for Modify, Soft Delete, Hard Delete buttons
-    // ...
 });
