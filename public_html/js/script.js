f72075fe1ca38da2f1a46f73d4b5cc3f7b8cd47e
ani <anirudh.margam@gmail.com>
2023-12-08 11:28:22 -0600

    maybe fixed student login

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -43,25 +43,37 @@ window.onclick = function(event) {
 
 // for student login
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
+    document.getElementById('studentLoginForm').addEventListener('submit', function(event) {
         event.preventDefault();
         var username = document.getElementById('inputUsername').value;
         var password = document.getElementById('inputPassword').value;
-        
-        // AJAX request to backend
-        
+        console.log('username', username);
+        console.log('password', password);
+
+        // Prepare form data using URLSearchParams
+        var formData = new URLSearchParams();
+        formData.append('inputUsername', username);
+        formData.append('inputPassword', password);
+
+        // AJAX request to backend        
         fetch('../php/student_login.php', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
+            body: formData
         })
         .then(response => response.json())
         .then(data => {
-            // Handle response here (e.g., redirect or show error)
-            sessionStorage.setItem('username', username);
-            window.location.href = './student_landing.html';
+            // Check response data for login success
+            if (data.success) {
+                // Store username in sessionStorage and redirect
+                sessionStorage.setItem('username', username);
+                window.location.href = './student_landing.html';
+            } else {
+                // If login failed, display the error message
+                alert(data.message);
+            }
         })
         .catch(error => {
             console.error('Error:', error);
