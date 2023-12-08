a9563e8b86719e78623e4652a2b5c68df94d8d9a
ani <anirudh.margam@gmail.com>
2023-12-08 16:45:02 -0600

    maybe done with student registration functionality

--- public_html/js/student_login_script.js
+++ public_html/js/student_login_script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         formData.append('inputUsername', username);
         formData.append('inputPassword', password);
 
-        // AJAX request to backend        
+        // AJAX request to backend
         fetch('../php/login.php', {
             method: 'POST',
             headers: {
@@ -25,7 +25,8 @@ document.addEventListener('DOMContentLoaded', function() {
             // Check response data for login success
             if (data.success) {
                 // Store username in sessionStorage and redirect
-                sessionStorage.setItem('username', username);
+                sessionStorage.setItem('username', data.username);
+                sessionStorage.setItem('uin', data.uin)
                 window.location.href = './student_landing.html';
             } else {
                 // If login failed, display the error message
@@ -35,5 +36,40 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => {
             console.error('Error:', error);
         });
+    });
+
+    // Event listener for the registration form
+    document.getElementById('studentRegistrationForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // Collect form data
+        var formData = new URLSearchParams(new FormData(this)); // 'this' refers to the registration form
+
+        // AJAX request to backend for registration
+        fetch('../php/registerStudent.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // Handle success (e.g., redirecting to login page or displaying a success message)
+                console.log(data.message);
+                // Optionally redirect to login page or show success message
+            } else {
+                // Handle error
+                console.error(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+
+    document.getElementById('registerButton').addEventListener('click', function() {
+        window.open('../components/student_register.html', '_blank'); // Open registration form in a new tab or window
     });
 });
