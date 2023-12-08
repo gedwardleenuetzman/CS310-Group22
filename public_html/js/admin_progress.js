7d7bfd7b2eee5849d499a19f84d75d6f489b648f
Jack Sanchez <texjack19@gmail.com>
2023-12-08 17:53:11 -0600

    more admin progress changes

--- public_html/js/admin_progress.js
+++ public_html/js/admin_progress.js
@@ -1,26 +1,62 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('../php/admin_progress.php') // PHP script that returns user data from the database
-    .then(response => response.json())
-    .then(enrollments => {
-        var enrollmentsTable = document.getElementById('enrollmentsTable');
-        enrollments.forEach(enrollment => {
-            var row = enrollmentsTable.insertRow();
-            row.innerHTML = `
-                <td>${enrollment.CE_Num}</td>
-                <td>${enrollment.UIN}</td>
-                <td>${enrollment.Class_ID}</td>
-                <td>${enrollment.Status}</td>
-                <td>${enrollment.Semester}</td>
-                <td>${enrollment.Year}</td>
-                <td>
-                    <button class="btn btn-primary btn-sm">Modify</button>
-                    <button class="btn btn-warning btn-sm">Soft Delete</button>
-                    <button class="btn btn-danger btn-sm">Hard Delete</button>
-                </td>
-            `;
+    fetch('../php/admin_progress.php')
+        .then(response => response.json())
+        .then(enrollments => {
+            var enrollmentsTable = document.getElementById('enrollmentsTable');
+            enrollments.forEach(enrollment => {
+                var row = enrollmentsTable.insertRow();
+                row.innerHTML = `
+                    <td>${enrollment.CE_Num}</td>
+                    <td>${enrollment.UIN}</td>
+                    <td>${enrollment.Class_ID}</td>
+                    <td>${enrollment.Status}</td>
+                    <td>${enrollment.Semester}</td>
+                    <td>${enrollment.Year}</td>
+                `;
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching class enrollments:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error fetching users:', error);
-    });
+
+    fetch('../php/admin_progress.php')
+        .then(response => response.json())
+        .then(certEnrollments => {
+            var certEnrollmentsTable = document.getElementById('certEnrollmentsTable');
+            certEnrollments.forEach(certEnrollment => {
+                var row = certEnrollmentsTable.insertRow();
+                row.innerHTML = `
+                    <td>${certEnrollment.CertE_Num}</td>
+                    <td>${certEnrollment.UIN}</td>
+                    <td>${certEnrollment.Cert_ID}</td>
+                    <td>${certEnrollment.Status}</td>
+                    <td>${certEnrollment.Training_Status}</td>
+                    <td>${certEnrollment.Program_Num}</td>
+                    <td>${certEnrollment.Semester}</td>
+                    <td>${certEnrollment.YEAR}</td>
+                `;
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching cert enrollments:', error);
+        });
+
+    fetch('../php/admin_progress.php')
+        .then(response => response.json())
+        .then(internApps => {
+            var internAppsTable = document.getElementById('internAppsTable');
+            internApps.forEach(internApp => {
+                var row = internAppsTable.insertRow();
+                row.innerHTML = `
+                    <td>${internApp.IA_Num}</td>
+                    <td>${internApp.UIN}</td>
+                    <td>${internApp.Intern_ID}</td>
+                    <td>${internApp.Status}</td>
+                    <td>${internApp.Year}</td>
+                `;
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching intern apps:', error);
+        });
 });
--- public_html/php/admin_progress.php
+++ public_html/php/admin_progress.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json');
 
 require_once './database_connection.php';
 
-// SQL query to fetch all users
+// SQL query to fetch all class enrollments
 $sql = "SELECT CE_Num, UIN, Class_ID, Status, Semester, Year FROM Class_Enrollment";
 
 $result = $conn->query($sql);
@@ -20,6 +20,40 @@ if ($result->num_rows > 0) {
         $enrollments[] = $row;
     }
     echo json_encode($enrollments);
+} else {
+    echo json_encode([]);
+}
+
+// SQL query to fetch all cert enrollments
+$sql = "SELECT CertE_Num, UIN, Cert_ID, Status, Training_Status, Program_Num, Semester, Year FROM Cert_Enrollment";
+
+$result = $conn->query($sql);
+
+$certEnrollments = array();
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $certEnrollments[] = $row;
+    }
+    echo json_encode($certEnrollments);
+} else {
+    echo json_encode([]);
+}
+
+// SQL query to fetch all intern apps
+$sql = "SELECT IA_Num, UIN, Intern_ID, Status, Year FROM Intern_App";
+
+$result = $conn->query($sql);
+
+$internApps = array();
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $internApps[] = $row;
+    }
+    echo json_encode($internApps);
 } else {
     echo json_encode([]);
 }
@@ -39,15 +73,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Progress utility functions
 
 function updateClassEnrollment() {
-    $updates = [];
-    foreach ($_POST as $key => $value) {
-        if ($key != 'update' && $key != 'delete' && $key != 'insert' && $key != $primary_key) {
-            // $updates[] = $conn->real_escape_string($key) . " = " . "'" . $conn->real_scape_string($value)
-        }
-    }
-    $updates = implode(", ", $updates);
-    $sql = "UPDATE {$table_name} SET {$updates} WHERE {$primary_key} = '{$_POST[$primary_key]}'";
-    $conn->query($sql);
+    // $updates = [];
+    // foreach ($_POST as $key => $value) {
+    //     if ($key != 'update' && $key != 'delete' && $key != 'insert' && $key != $primary_key) {
+    //         // $updates[] = $conn->real_escape_string($key) . " = " . "'" . $conn->real_scape_string($value)
+    //     }
+    // }
+    // $updates = implode(", ", $updates);
+    // $sql = "UPDATE {$table_name} SET {$updates} WHERE {$primary_key} = '{$_POST[$primary_key]}'";
+    // $conn->query($sql);
 }
 
 function deleteClassEnrollment() {
@@ -57,21 +91,21 @@ function deleteClassEnrollment() {
 
 function insertClassEnrollment() {
     // Insert new row
-    $columns = [];
-    $values = [];
+    // $columns = [];
+    // $values = [];
 
-    foreach ($POST as $key => $value) {
-        if ($key != 'update' && $key != 'delete' && $key != 'insert' && $key != $primary_key) {
-            $columns[] = $conn->real_escape_string($key);
-            $values[] = "'" . $conn->real_escape_string($value);
-        }
-    }
+    // foreach ($POST as $key => $value) {
+    //     if ($key != 'update' && $key != 'delete' && $key != 'insert' && $key != $primary_key) {
+    //         $columns[] = $conn->real_escape_string($key);
+    //         $values[] = "'" . $conn->real_escape_string($value);
+    //     }
+    // }
 
-    $columns = implode(", ", $columns);
-    $values = implode(", ", $values);
+    // $columns = implode(", ", $columns);
+    // $values = implode(", ", $values);
 
-    $sql = "INSERT INTO {$table_name} VALUES {$columns} VALUES '{$values}'";
-    $conn->query($sql);
+    // $sql = "INSERT INTO {$table_name} VALUES {$columns} VALUES '{$values}'";
+    // $conn->query($sql);
 }
 
 $conn->close();
