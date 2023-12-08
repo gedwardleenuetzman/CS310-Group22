92b37d4c55a4dface6dfcdd68e69c04ea0926560
Braeden Smith <braedensmith629@gmail.com>
2023-12-08 14:17:27 -0600

    Added select for programs

---
+++ public_html/js/admin_programs.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+  fetch(`/../php/fetch_programs.php`)
+    .then(response => response.json())
+    .then(data => {
+      var programDiv = document.getElementById('programs');
+
+      // Create a table element
+      var table = document.createElement('table');
+      table.classList.add('program-table');
+
+      // Create table header row
+      var headerRow = table.insertRow(0);
+      headerRow.innerHTML = '<th>Name</th><th>Description</th><th>Edit</th><th>Delete</th>';
+
+      // Populate the table with program data
+      data.forEach(program => {
+        var row = table.insertRow();
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = program.Name;
+
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = program.Description;
+
+        var cell3 = row.insertCell(2);
+        var editButton = document.createElement('button');
+        editButton.innerText = 'Edit';
+        editButton.addEventListener('click', function() {
+          // Edit stuff
+        });
+        cell3.appendChild(editButton);
+
+        var cell4 = row.insertCell(3);
+        var deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.addEventListener('click', function() {
+          // Delete stuff
+        });
+        cell4.appendChild(deleteButton);
+      });
+
+      // Append the table to the programDiv
+      programDiv.appendChild(table);
+    })
+    .catch(error => {
+      console.error('Error fetching documents:', error);
+    });
+});
