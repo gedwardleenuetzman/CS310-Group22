873c98f3af831a0e753558a79f352cf5864a885d
ani <anirudh.margam@gmail.com>
2023-12-08 12:18:08 -0600

    fixed js problems, modularized

---
+++ public_html/js/admin_users_script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('/../php/fetch_admin_users.php') // PHP script that returns user data from the database
+    .then(response => response.json())
+    .then(users => {
+        var usersTable = document.getElementById('usersTable');
+        users.forEach(user => {
+            var row = usersTable.insertRow();
+            row.innerHTML = `
+                <td>${user.First_Name}</td>
+                <td>${user.M_Initial}</td>
+                <td>${user.Last_Name}</td>
+                <td>${user.Username}</td>
+                <td>${user.Email}</td>
+                <td>${user.Discord_Name}</td>
+                <td>
+                    <button class="btn btn-primary btn-sm">Modify</button>
+                    <button class="btn btn-warning btn-sm">Soft Delete</button>
+                    <button class="btn btn-danger btn-sm">Hard Delete</button>
+                </td>
+            `;
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching users:', error);
+    });
+});
